02572aea781b8542daca128d4f256909354eb253
Andres Sebastian Lazarte <sebalazarte@gmail.com>
2021-06-16 20:32:23 -0300

    inicio del proyecto

---
+++ client/src/MapasApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MapasApp = () => {
+    return (
+        <div>
+            Hola mundo
+        </div>
+    )
+}
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import { MapasApp } from './MapasApp';
+
+reactDom.render(
+    <MapasApp/>,
+    document.getElementById('root')
+);
